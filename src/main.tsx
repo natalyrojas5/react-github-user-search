13ace2b72caa6040d703b94e88993a9a3361867c
Nataly Rojas <natalyrojasmoreno@Nataly-Rojas-2.local>
2022-07-12 00:15:05 -0500

    ADD: File main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <h1>Hello World</h1>
+  </StrictMode>
+);
