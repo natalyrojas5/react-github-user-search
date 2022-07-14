2716c56393fd0946b1a4dc78c0f79fe51cf95ac2
Nataly Rojas <natalyrojasmoreno@Nataly-Rojas-2.local>
2022-07-14 17:38:38 -0500

    UPDATE [STYLES]: search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,13 +4,13 @@ const Search = () => {
   return (
     <div className="search bg-secondary border-r-06">
       <div className="search__icon">
-        <i className="fa-solid fa-magnifying-glass text-primary cursor-pointer"></i>
+        <i className="fa-solid fa-magnifying-glass text-primary"></i>
       </div>
       <div className="search__input">
         <input type="text" placeholder="Search GitHub username ..." />
       </div>
       <div className="search__btn">
-        <button type="button" className="border-r-06 cursor-pointer">
+        <button type="button" className="border-r-06">
           search
         </button>
       </div>
