de07f68a8a430f27a2c44c8ac3efb020bdba6ccd
Nataly Rojas <natalyrojasmoreno@Nataly-Rojas-2.local>
2022-07-18 23:35:56 -0500

    ADD: Skeletib for component CardUserBio

--- src/components/CardUserBio/CardUserBio.tsx
+++ src/components/CardUserBio/CardUserBio.tsx
@@ -1,4 +1,9 @@
-const CardUserBio = () => {
+import { SkeletonElement } from "../../components/Skeleton";
+
+const CardUserBio = (isLoading = true) => {
+  if (isLoading) {
+    return <SkeletonElement wrapperStyles={{ height: "1.2rem" }} />;
+  }
   return <p className="card-user-bio text-gray-100">This profile has no bio</p>;
 };
 
