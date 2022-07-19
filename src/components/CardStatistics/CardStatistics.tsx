bd4b293b1ae00a27c72bfd8d3d3baa768d19e9d2
Nataly Rojas <natalyrojasmoreno@Nataly-Rojas-2.local>
2022-07-18 23:35:39 -0500

    ADD: Skeletib for component CardStatistics

--- src/components/CardStatistics/CardStatistics.tsx
+++ src/components/CardStatistics/CardStatistics.tsx
@@ -1,19 +1,38 @@
+import { SkeletonElement } from "../../components/Skeleton";
 import "./CardStatistics.scss";
 
-const CardStatistics = () => {
+const CardStatistics = (isLoading = true) => {
   return (
     <div className="card-statistics bg-primary border-r-06">
       <div className="card-statistics__repos">
         <h2 className="text-gray">Repos</h2>
-        <p>0</p>
+        {isLoading ? (
+          <SkeletonElement
+            wrapperStyles={{ height: "1.5rem", width: "3rem" }}
+          />
+        ) : (
+          <p>0</p>
+        )}
       </div>
       <div className="card-statistics__followers">
         <h2 className="text-gray">Followers</h2>
-        <p>0</p>
+        {isLoading ? (
+          <SkeletonElement
+            wrapperStyles={{ height: "1.5rem", width: "3rem" }}
+          />
+        ) : (
+          <p>0</p>
+        )}
       </div>
       <div className="card-statistics__following">
         <h2 className="text-gray">Following</h2>
-        <p>0</p>
+        {isLoading ? (
+          <SkeletonElement
+            wrapperStyles={{ height: "1.5rem", width: "3rem" }}
+          />
+        ) : (
+          <p>0</p>
+        )}
       </div>
     </div>
   );
